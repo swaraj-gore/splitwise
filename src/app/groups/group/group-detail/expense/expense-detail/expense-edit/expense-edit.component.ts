a8f1a4ca510f34f595d110f0a50191b9260b7dc3
Swaraj Gore <swarajgore99@gmail.com>
2023-06-06 12:53:14 +0530

    Add expense-edit component

---
+++ src/app/groups/group/group-detail/expense/expense-detail/expense-edit/expense-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-edit',
+  templateUrl: './expense-edit.component.html',
+  styleUrls: ['./expense-edit.component.css']
+})
+export class ExpenseEditComponent {
+
+}
