7992a1a3568774e4d602a3a5d736b1697f790413
Swaraj Gore <swarajgore99@gmail.com>
2023-06-07 11:01:10 +0530

    Create Shared group form component

---
+++ src/app/shared/group-form/group-form.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router, UrlSegment } from '@angular/router';
+import { Group } from 'src/app/model/group.model';
+import { GroupService } from 'src/app/service/group.service';
+
+@Component({
+  selector: 'app-group-form',
+  templateUrl: './group-form.component.html',
+  styleUrls: ['./group-form.component.css']
+})
+export class GroupFormComponent {
+  groupForm: FormGroup;
+  isEdit = true;
+  formHeading = "Edit Group";
+  groupId: number;
+  group: Group;
+  constructor(private groupService: GroupService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.initForm();
+    this.route.url.subscribe(
+      (url: UrlSegment[]) => {
+        if(url[0].path === "new") {
+          this.isEdit = false;
+          this.formHeading = "Add Group";
+        }
+        else {
+          this.route.parent?.params.subscribe(
+            (params: Params) => {
+              this.groupId = +params['id'];
+              this.groupService.getGroupById(this.groupId).subscribe(
+                (group: Group) => {
+                  this.group = group;
+                  this.initForm(this.group.name);
+                }
+              );
+            }
+          )
+        }
+      }
+    )
+  }
+
+  initForm(groupName=null) {
+    this.groupForm = new FormGroup({
+      'name': new FormControl(groupName, [
+        Validators.required,
+      ])
+    })
+  }
+
+  isEqual() {
+    return this.group && this.group.name === this.groupForm.value.name;
+  }
+
+  onSubmit() {
+    if(!this.isEdit) 
+      this.groupService.addGroup(this.groupForm.value).subscribe(
+        () => this.groupService.groupsChanged.next()
+      );
+    else {
+      this.groupService.updateGroup(this.groupId, this.groupForm.value.name).subscribe(
+        () => this.groupService.groupsChanged.next()
+      );
+    }
+    this.goBack()
+  }
+
+  onClear() {
+    this.groupForm.reset()
+  }
+
+  onReset() {
+    this.isEdit ? this.initForm(this.group.name) : this.groupForm.reset();
+  }
+
+  goBack() {
+    this.router.navigate(['../'], {relativeTo: this.route})
+  }
+}
