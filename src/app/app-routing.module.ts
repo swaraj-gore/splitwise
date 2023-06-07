3609717e3973094aeb316bbf42fea49260d5d82a
Swaraj Gore <swarajgore99@gmail.com>
2023-06-07 12:11:17 +0530

    Use form components instead of edit and add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Route, RouterModule } from "@angular/router";
 import { GroupsComponent } from "./groups/groups.component";
-import { AddGroupComponent } from "./groups/add-group/add-group.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { AuthComponent } from "./auth/auth.component";
 import { canActiate } from "./auth/auth.guard";
@@ -9,14 +8,16 @@ import { GroupDetailComponent } from "./groups/group/group-detail/group-detail.c
 import { GroupSummaryComponent } from "./groups/group/group-detail/group-summary/group-summary.component";
 import { ManageMembersComponent } from "./groups/group/group-detail/manage-members/manage-members.component";
 import { ExpenseDetailComponent } from "./groups/group/group-detail/expense/expense-detail/expense-detail.component";
-import { ExpenseEditComponent } from "./groups/group/group-detail/expense/expense-detail/expense-edit/expense-edit.component";
+import { ExpenseFormComponent } from "./shared/expense-form/expense-form.component";
+import { GroupFormComponent } from "./shared/group-form/group-form.component";
 
 
 const appRoutes: Route[] = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent, canActivate: [canActiate]},
   { path: 'groups', component: GroupsComponent, canActivate: [canActiate], children: [
-    { path: 'new', component: AddGroupComponent },
+    { path: 'new-group', component: GroupFormComponent },
+    { path: 'new-expense', component: ExpenseFormComponent},
     { path: ':id', component: GroupDetailComponent, children: [
         { path: '', redirectTo: 'summary', pathMatch: 'full' },
         { path: 'summary', component: GroupSummaryComponent },
@@ -24,10 +25,11 @@ const appRoutes: Route[] = [
         { path: 'expense', children: [
             { path: ':expenseId', children: [
               { path: '', component: ExpenseDetailComponent},
-              { path: 'edit', component: ExpenseEditComponent}
-            ]},
+              { path: 'edit', component: ExpenseFormComponent}
+            ]}
           ]
         },
+        { path: 'edit', component: GroupFormComponent}
       ]
     }
   ]},
